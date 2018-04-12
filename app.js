6ce48ad7033701b64333d9ad3281b6431e85eed7
Josh Stewart <jrbs1120@gmail.com>
2018-04-12 05:40:14 +0000

    first deploy

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+    res.render("landing.ejs");
+});
+
+app.get("/media", function(req, res) {
+    res.render("media.ejs");
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Backroute Server has Started");
+});
