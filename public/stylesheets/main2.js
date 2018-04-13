62ea2f27f26a21ac76badb40ae4c7e1975aa5c9d
Josh Stewart <jrbs1120@gmail.com>
2018-04-13 06:11:04 +0000

    fixed mobile

--- public/stylesheets/main2.js
+++ public/stylesheets/main2.js
@@ -21,3 +21,9 @@ function onPlayerReady(event) {
     event.target.setVolume(0);
     event.target.playVideo();
 }
+
+
+var siteWidth = 1280;
+var scale = screen.width / siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=' + siteWidth + ', initial-scale=' + scale + '');
