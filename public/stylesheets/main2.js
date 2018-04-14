457a9646093f9fb9eef1727e082d4f045e2ed4e6
Josh Stewart <jrbs1120@gmail.com>
2018-04-14 01:56:55 +0000

    hamburger updated

--- public/stylesheets/main2.js
+++ public/stylesheets/main2.js
@@ -23,7 +23,7 @@ function onPlayerReady(event) {
 }
 
 
-var siteWidth = 1280;
-var scale = screen.width / siteWidth;
+// var siteWidth = 1280;
+// var scale = screen.width / siteWidth;
 
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=' + siteWidth + ', initial-scale=' + scale + '');
+// document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=' + siteWidth + ', initial-scale=' + scale + '');
