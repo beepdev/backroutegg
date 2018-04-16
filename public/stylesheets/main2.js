dccccff224914c980c0d44877e21cd503bbfde0d
Josh Stewart <jrbs1120@gmail.com>
2018-04-16 01:48:28 +0000

    fixed mobile

--- public/stylesheets/main2.js
+++ public/stylesheets/main2.js
@@ -22,12 +22,16 @@ function onPlayerReady(event) {
     event.target.playVideo();
 }
 
-        //         Facebook javascript
-        //         <div id="fb-root"></div>
-        // <script>(function(d, s, id) {
-        //   var js, fjs = d.getElementsByTagName(s)[0];
-        //   if (d.getElementById(id)) return;
-        //   js = d.createElement(s); js.id = id;
-        //   js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=1697881106964639&autoLogAppEvents=1';
-        //   fjs.parentNode.insertBefore(js, fjs);
-        // }(document, 'script', 'facebook-jssdk'));</script>
+// PayPal Checkout Script
+
+
+
+//         Facebook javascript
+//         <div id="fb-root"></div>
+// <script>(function(d, s, id) {
+//   var js, fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) return;
+//   js = d.createElement(s); js.id = id;
+//   js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=1697881106964639&autoLogAppEvents=1';
+//   fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));</script>
