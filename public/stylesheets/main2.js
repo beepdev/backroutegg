9c17f2b23e2d02e9c6e40ade96b5097bedadf856
Josh Stewart <jrbs1120@gmail.com>
2018-04-26 04:19:02 +0000

    front page changes

--- public/stylesheets/main2.js
+++ public/stylesheets/main2.js
@@ -32,3 +32,6 @@
 //   js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=1697881106964639&autoLogAppEvents=1';
 //   fjs.parentNode.insertBefore(js, fjs);
 // }(document, 'script', 'facebook-jssdk'));</script>
+
+
+// jQuery Commands
