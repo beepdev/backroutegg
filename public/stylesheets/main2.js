4d5500746c2f09e0038655da0073eeabd18fd72e
Josh Stewart <jrbs1120@gmail.com>
2018-04-29 21:43:32 +0000

    front page changes

--- public/stylesheets/main2.js
+++ public/stylesheets/main2.js
@@ -38,3 +38,12 @@
 
 
 // jQuery Commands
+var $ = jQuery;
+
+$(document).ready(function() {
+    $('#zoom')
+        .wrap('<span style="display:inline-block"></span>')
+        .css('display', 'block')
+        .parent()
+        .zoom();
+});
